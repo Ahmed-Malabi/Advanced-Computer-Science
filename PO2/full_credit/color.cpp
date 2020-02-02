c0d4c4da558015b5c446c8f5c0d0b148a7e81166
Ahmed <ahmed.n.malabi@gmail.com>
2020-02-02 16:58:32 -0600

    Verious changes including attempt 1 at a make file and Main

--- PO2/full_credit/color.cpp
+++ PO2/full_credit/color.cpp
@@ -9,18 +9,22 @@ class Color
 	{
 		if((red < 0 || red > 255) || (green < 0 || green > 255) || (blue < 0 || blue > 255))
 		{
-			
+			std::cerr << "One of the values you entered was out of bounds" <<endl;
+			exit();
 		}
 	}
 	Color::~Color();
+	
 	std::String Color::toString()
 	{
-		return cout << std::toString(_red) + ";" + std::toString(_green) + ";" + std::toString(_blue);
+		return cout << "(" + std::toString(_red) + "," + std::toString(_green) + "," + std::toString(_blue) + ")";
 	}
+	
 	std::string colorize(std::string text)
 	{
-		
+		return "\033[38;2;" + _red + ";" + _green + ";" + _blue + ";177m " + text + "\033[0m";
 	}
+	
   private:
 	int _red, _green, _blue;
 }

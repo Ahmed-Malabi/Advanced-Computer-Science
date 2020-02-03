1759cdeb6ac611e2d4be50a7c1137d496f1fc6d3
Ahmed <ahmed.n.malabi@gmail.com>
2020-02-03 13:51:54 -0600

    fixed color.cpp

--- PO2/full_credit/color.cpp
+++ PO2/full_credit/color.cpp
@@ -2,29 +2,25 @@
 #include <iostream>
 #include <string>
 
-class Color 
+
+
+Color::Color(int red, int green, int blue) _red{red}, _green{green}, _blue{blue}
 {
-  public:
-	Color::Color(int red, int green, int blue) _red(red), _green(green), _blue(blue)
-	{
-		if((red < 0 || red > 255) || (green < 0 || green > 255) || (blue < 0 || blue > 255))
-		{
-			std::cerr << "One of the values you entered was out of bounds" <<endl;
-			exit();
-		}
-	}
-	Color::~Color();
-	
-	std::String Color::toString()
-	{
-		return cout << "(" + std::toString(_red) + "," + std::toString(_green) + "," + std::toString(_blue) + ")";
-	}
-	
-	std::string colorize(std::string text)
-	{
-		return "\033[38;2;" + _red + ";" + _green + ";" + _blue + ";177m " + text + "\033[0m";
-	}
-	
-  private:
 	int _red, _green, _blue;
+	if((red < 0 || red > 255) || (green < 0 || green > 255) || (blue < 0 || blue > 255))
+	{
+		std::cerr << "One of the values you entered was out of bounds" <<endl;
+		exit();
+	}
 }
+
+std::String Color::toString()
+{
+	return cout << "(" + std::toString(_red) + "," + std::toString(_green) + "," + std::toString(_blue) + ")";
+}
+
+std::string colorize(std::string text)
+{
+	return "\033[38;2;" + _red + ";" + _green + ";" + _blue + ";177m " + text + "\033[0m";
+}
+

3621139829c3153b87eb138c6666c2bed776e370
Ahmed <ahmed.n.malabi@gmail.com>
2020-02-25 00:31:40 -0600

    I tried

--- P05/bonus/test.cpp
+++ P05/bonus/test.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <sstream>
 #include <assert.h> 
 #include "Product.h"
 #include "Taxfree.h"
@@ -9,16 +10,16 @@
 
 int main()
 {
-	std::string test;
+	std::ostringstream test;
 	Taxfree tfTest{"milk", 2.45};
 	tfTest.Product::set_quantity(-1);
 	test << tfTest;
-	assert(test == "milk ($2.45)");
-	assert(tfTest.Taxfree::price() == -2.45)
+	assert((test.std::compare("milk ($2.45)")));
+	assert(tfTest.Taxfree::price() == -2.45);
 	
 	Taxed taxedTest{"egg", 2.25};
 	Taxed::set_tax_rate(0.0725);
 	test << taxedTest;
-	assert(test == "egg ($2.25)");
-	assert(taxedTest.Taxed::price() == -2.41)
+	assert(test.std::compare("egg ($2.25)")));
+	assert(taxedTest.Taxed::price() == -2.41);
 }
